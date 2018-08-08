7e03cf360a5910ae9f54e6c56225d9c79d13deae
sthai1721 <sophia.thai@rutgers.edu>
2018-08-08 14:56:28 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -37,8 +37,9 @@ def how_many_steps?
 steps=0
 
 loop do 
+    steps=steps+1
   puts steps
-  steps=steps+1
+
 
 if steps%2==0
   puts "Left"
@@ -48,6 +49,7 @@ end
 sleep(0.5)
 break
 end
+
 end
 
 def break_dance
